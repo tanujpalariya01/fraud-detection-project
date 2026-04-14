10774e6189ace6593c5f27e07b6b18ff2147f902
Tanujpalariya01 <palariyatanuj9@gmail.com>
2026-04-14 18:23:24 +0530

    merge

--- train_model.py
+++ train_model.py
@@ -1,7 +1,51 @@
+# from sklearn.metrics import accuracy_score
+# from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+# import pandas as pd
+# from sklearn.model_selection import train_test_split
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.metrics import classification_report
+# import pickle
+
+# # Load dataset
+# data = pd.read_csv("creditcard.csv")
+
+# # Split features and label
+# X = data.drop("Class", axis=1)
+# y = data["Class"]
+
+# # Train-test split (important for imbalance)
+# X_train, X_test, y_train, y_test = train_test_split(
+#     X, y, test_size=0.2, random_state=42, stratify=y
+# )
+
+# # # Train Logistic Regression model
+# model = LogisticRegression(max_iter=1000, class_weight="balanced")
+# model.fit(X_train, y_train)
+# y_pred = model.predict(X_test)
+# accuracy = accuracy_score(y_test, y_pred)
+# print("Accuracy:", accuracy)
+# # Precision, Recall, F1-score
+# print("\nClassification Report:")
+# print(classification_report(y_test, y_pred))
+
+# # Confusion Matrix
+# cm = confusion_matrix(y_test, y_pred)
+# print("\nConfusion Matrix:")
+# print(cm)
+
+
+# # Evaluate model
+# y_pred = model.predict(X_test)
+# print(classification_report(y_test, y_pred))
+
+# # Save model
+# pickle.dump(model, open("model.pkl", "wb"))
+# print("✅ model.pkl created successfully")
+
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import pickle
 
 # Load dataset
@@ -11,7 +55,7 @@ data = pd.read_csv("creditcard.csv")
 X = data.drop("Class", axis=1)
 y = data["Class"]
 
-# Train-test split (important for imbalance)
+# Train-test split
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y
 )
@@ -20,9 +64,21 @@ X_train, X_test, y_train, y_test = train_test_split(
 model = LogisticRegression(max_iter=1000, class_weight="balanced")
 model.fit(X_train, y_train)
 
-# Evaluate model
+# Prediction
 y_pred = model.predict(X_test)
+
+# Accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy)
+
+# Precision, Recall, F1-score
+print("\nClassification Report:")
 print(classification_report(y_test, y_pred))
+
+# Confusion Matrix
+cm = confusion_matrix(y_test, y_pred)
+print("\nConfusion Matrix:")
+print(cm)
 
 # Save model
 pickle.dump(model, open("model.pkl", "wb"))
